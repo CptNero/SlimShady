3ff1c6ee441a56ea3e1bade0432d86ea8788873f
Adrian Ravai <ravaiadrian@gmail.com>
2020-09-19 11:51:29 +0200

    Add cmake-build-debug folder

---
+++ cmake-build-debug/CMakeFiles/SlimShady.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/SlimShady.dir/main.cpp.obj"
+  "SlimShady.exe"
+  "SlimShady.exe.manifest"
+  "SlimShady.lib"
+  "SlimShady.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/SlimShady.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
