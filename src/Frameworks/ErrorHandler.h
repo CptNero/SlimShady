d6027e4bdc7ab8260183ddf899ff8f59b5128c7e
CptNero <ravaiadrian@gmail.com>
2020-10-30 17:22:03 +0100

    Added frameworks folder to project.

---
+++ src/Frameworks/ErrorHandler.h
@@ -0,0 +1,21 @@
+#ifndef OPENGLTEMPLATE_ERRORHANDLER_H
+#define OPENGLTEMPLATE_ERRORHANDLER_H
+
+#include <glew.h>
+
+class ErrorHandler {
+  public:
+    ErrorHandler();
+    ~ErrorHandler();
+
+    static void GLAPIENTRY MessageCallback(
+      GLenum source,
+      GLenum type,
+      GLuint id,
+      GLenum severity,
+      GLsizei length,
+      const GLchar* message,
+      const void* userParam);
+};
+
+#endif //OPENGLTEMPLATE_ERRORHANDLER_H
