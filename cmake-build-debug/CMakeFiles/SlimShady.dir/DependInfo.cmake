acd3e504dd8f439dd1191b43f627f26c29bfb4a1
Ravai Adrian <ravaiadrian@gmail.com>
2021-05-20 17:39:13 +0200

    Just push everything then remove it later.

--- cmake-build-debug/CMakeFiles/SlimShady.dir/DependInfo.cmake
+++ cmake-build-debug/CMakeFiles/SlimShady.dir/DependInfo.cmake
@@ -4,45 +4,46 @@ set(CMAKE_DEPENDS_LANGUAGES
   )
 # The set of files for implicit dependencies of each language:
 set(CMAKE_DEPENDS_CHECK_CXX
-  "C:/Users/Neresis/CLionProjects/SlimShady/src/App.cpp" "C:/Users/Neresis/CLionProjects/SlimShady/cmake-build-debug/CMakeFiles/SlimShady.dir/src/App.cpp.obj"
-  "C:/Users/Neresis/CLionProjects/SlimShady/src/Frameworks/Clock.cpp" "C:/Users/Neresis/CLionProjects/SlimShady/cmake-build-debug/CMakeFiles/SlimShady.dir/src/Frameworks/Clock.cpp.obj"
-  "C:/Users/Neresis/CLionProjects/SlimShady/src/Frameworks/Configurations.cpp" "C:/Users/Neresis/CLionProjects/SlimShady/cmake-build-debug/CMakeFiles/SlimShady.dir/src/Frameworks/Configurations.cpp.obj"
-  "C:/Users/Neresis/CLionProjects/SlimShady/src/Frameworks/ErrorHandler.cpp" "C:/Users/Neresis/CLionProjects/SlimShady/cmake-build-debug/CMakeFiles/SlimShady.dir/src/Frameworks/ErrorHandler.cpp.obj"
-  "C:/Users/Neresis/CLionProjects/SlimShady/src/Frameworks/ShaderFileManager.cpp" "C:/Users/Neresis/CLionProjects/SlimShady/cmake-build-debug/CMakeFiles/SlimShady.dir/src/Frameworks/ShaderFileManager.cpp.obj"
-  "C:/Users/Neresis/CLionProjects/SlimShady/src/IndexBuffer.cpp" "C:/Users/Neresis/CLionProjects/SlimShady/cmake-build-debug/CMakeFiles/SlimShady.dir/src/IndexBuffer.cpp.obj"
-  "C:/Users/Neresis/CLionProjects/SlimShady/src/Renderer.cpp" "C:/Users/Neresis/CLionProjects/SlimShady/cmake-build-debug/CMakeFiles/SlimShady.dir/src/Renderer.cpp.obj"
-  "C:/Users/Neresis/CLionProjects/SlimShady/src/SceneElement.cpp" "C:/Users/Neresis/CLionProjects/SlimShady/cmake-build-debug/CMakeFiles/SlimShady.dir/src/SceneElement.cpp.obj"
-  "C:/Users/Neresis/CLionProjects/SlimShady/src/SceneLoader.cpp" "C:/Users/Neresis/CLionProjects/SlimShady/cmake-build-debug/CMakeFiles/SlimShady.dir/src/SceneLoader.cpp.obj"
-  "C:/Users/Neresis/CLionProjects/SlimShady/src/Shader.cpp" "C:/Users/Neresis/CLionProjects/SlimShady/cmake-build-debug/CMakeFiles/SlimShady.dir/src/Shader.cpp.obj"
-  "C:/Users/Neresis/CLionProjects/SlimShady/src/VertexArray.cpp" "C:/Users/Neresis/CLionProjects/SlimShady/cmake-build-debug/CMakeFiles/SlimShady.dir/src/VertexArray.cpp.obj"
-  "C:/Users/Neresis/CLionProjects/SlimShady/src/VertexBuffer.cpp" "C:/Users/Neresis/CLionProjects/SlimShady/cmake-build-debug/CMakeFiles/SlimShady.dir/src/VertexBuffer.cpp.obj"
-  "C:/Users/Neresis/CLionProjects/SlimShady/src/VertexBufferLayout.cpp" "C:/Users/Neresis/CLionProjects/SlimShady/cmake-build-debug/CMakeFiles/SlimShady.dir/src/VertexBufferLayout.cpp.obj"
-  "C:/Users/Neresis/CLionProjects/SlimShady/src/Widgets/ConsoleWidget.cpp" "C:/Users/Neresis/CLionProjects/SlimShady/cmake-build-debug/CMakeFiles/SlimShady.dir/src/Widgets/ConsoleWidget.cpp.obj"
-  "C:/Users/Neresis/CLionProjects/SlimShady/src/Widgets/SceneEditorWidget.cpp" "C:/Users/Neresis/CLionProjects/SlimShady/cmake-build-debug/CMakeFiles/SlimShady.dir/src/Widgets/SceneEditorWidget.cpp.obj"
-  "C:/Users/Neresis/CLionProjects/SlimShady/src/Widgets/TextEditorWidget.cpp" "C:/Users/Neresis/CLionProjects/SlimShady/cmake-build-debug/CMakeFiles/SlimShady.dir/src/Widgets/TextEditorWidget.cpp.obj"
-  "C:/Users/Neresis/CLionProjects/SlimShady/src/Widgets/Widget.cpp" "C:/Users/Neresis/CLionProjects/SlimShady/cmake-build-debug/CMakeFiles/SlimShady.dir/src/Widgets/Widget.cpp.obj"
-  "C:/Users/Neresis/CLionProjects/SlimShady/Vendor/ImGuiColorTextEdit/TextEditor.cpp" "C:/Users/Neresis/CLionProjects/SlimShady/cmake-build-debug/CMakeFiles/SlimShady.dir/vendor/ImGuiColorTextEdit/TextEditor.cpp.obj"
-  "C:/Users/Neresis/CLionProjects/SlimShady/Vendor/imgui/imgui.cpp" "C:/Users/Neresis/CLionProjects/SlimShady/cmake-build-debug/CMakeFiles/SlimShady.dir/vendor/imgui/imgui.cpp.obj"
-  "C:/Users/Neresis/CLionProjects/SlimShady/Vendor/imgui/imgui_draw.cpp" "C:/Users/Neresis/CLionProjects/SlimShady/cmake-build-debug/CMakeFiles/SlimShady.dir/vendor/imgui/imgui_draw.cpp.obj"
-  "C:/Users/Neresis/CLionProjects/SlimShady/Vendor/imgui/imgui_impl_glfw.cpp" "C:/Users/Neresis/CLionProjects/SlimShady/cmake-build-debug/CMakeFiles/SlimShady.dir/vendor/imgui/imgui_impl_glfw.cpp.obj"
-  "C:/Users/Neresis/CLionProjects/SlimShady/Vendor/imgui/imgui_impl_opengl3.cpp" "C:/Users/Neresis/CLionProjects/SlimShady/cmake-build-debug/CMakeFiles/SlimShady.dir/vendor/imgui/imgui_impl_opengl3.cpp.obj"
-  "C:/Users/Neresis/CLionProjects/SlimShady/Vendor/imgui/imgui_stdlib.cpp" "C:/Users/Neresis/CLionProjects/SlimShady/cmake-build-debug/CMakeFiles/SlimShady.dir/vendor/imgui/imgui_stdlib.cpp.obj"
-  "C:/Users/Neresis/CLionProjects/SlimShady/Vendor/imgui/imgui_widgets.cpp" "C:/Users/Neresis/CLionProjects/SlimShady/cmake-build-debug/CMakeFiles/SlimShady.dir/vendor/imgui/imgui_widgets.cpp.obj"
+  "/home/neresis/Projects/Personal/SlimShady/Vendor/ImGuiColorTextEdit/TextEditor.cpp" "/home/neresis/Projects/Personal/SlimShady/cmake-build-debug/CMakeFiles/SlimShady.dir/Vendor/ImGuiColorTextEdit/TextEditor.cpp.o"
+  "/home/neresis/Projects/Personal/SlimShady/Vendor/imgui/imgui.cpp" "/home/neresis/Projects/Personal/SlimShady/cmake-build-debug/CMakeFiles/SlimShady.dir/Vendor/imgui/imgui.cpp.o"
+  "/home/neresis/Projects/Personal/SlimShady/Vendor/imgui/imgui_draw.cpp" "/home/neresis/Projects/Personal/SlimShady/cmake-build-debug/CMakeFiles/SlimShady.dir/Vendor/imgui/imgui_draw.cpp.o"
+  "/home/neresis/Projects/Personal/SlimShady/Vendor/imgui/imgui_impl_glfw.cpp" "/home/neresis/Projects/Personal/SlimShady/cmake-build-debug/CMakeFiles/SlimShady.dir/Vendor/imgui/imgui_impl_glfw.cpp.o"
+  "/home/neresis/Projects/Personal/SlimShady/Vendor/imgui/imgui_impl_opengl3.cpp" "/home/neresis/Projects/Personal/SlimShady/cmake-build-debug/CMakeFiles/SlimShady.dir/Vendor/imgui/imgui_impl_opengl3.cpp.o"
+  "/home/neresis/Projects/Personal/SlimShady/Vendor/imgui/imgui_stdlib.cpp" "/home/neresis/Projects/Personal/SlimShady/cmake-build-debug/CMakeFiles/SlimShady.dir/Vendor/imgui/imgui_stdlib.cpp.o"
+  "/home/neresis/Projects/Personal/SlimShady/Vendor/imgui/imgui_widgets.cpp" "/home/neresis/Projects/Personal/SlimShady/cmake-build-debug/CMakeFiles/SlimShady.dir/Vendor/imgui/imgui_widgets.cpp.o"
+  "/home/neresis/Projects/Personal/SlimShady/Vendor/stb_image/stb_image.cpp" "/home/neresis/Projects/Personal/SlimShady/cmake-build-debug/CMakeFiles/SlimShady.dir/Vendor/stb_image/stb_image.cpp.o"
+  "/home/neresis/Projects/Personal/SlimShady/Vendor/stb_image/stb_image_write.cpp" "/home/neresis/Projects/Personal/SlimShady/cmake-build-debug/CMakeFiles/SlimShady.dir/Vendor/stb_image/stb_image_write.cpp.o"
+  "/home/neresis/Projects/Personal/SlimShady/src/App.cpp" "/home/neresis/Projects/Personal/SlimShady/cmake-build-debug/CMakeFiles/SlimShady.dir/src/App.cpp.o"
+  "/home/neresis/Projects/Personal/SlimShady/src/Camera.cpp" "/home/neresis/Projects/Personal/SlimShady/cmake-build-debug/CMakeFiles/SlimShady.dir/src/Camera.cpp.o"
+  "/home/neresis/Projects/Personal/SlimShady/src/FrameBuffer.cpp" "/home/neresis/Projects/Personal/SlimShady/cmake-build-debug/CMakeFiles/SlimShady.dir/src/FrameBuffer.cpp.o"
+  "/home/neresis/Projects/Personal/SlimShady/src/Frameworks/Clock.cpp" "/home/neresis/Projects/Personal/SlimShady/cmake-build-debug/CMakeFiles/SlimShady.dir/src/Frameworks/Clock.cpp.o"
+  "/home/neresis/Projects/Personal/SlimShady/src/Frameworks/Context.cpp" "/home/neresis/Projects/Personal/SlimShady/cmake-build-debug/CMakeFiles/SlimShady.dir/src/Frameworks/Context.cpp.o"
+  "/home/neresis/Projects/Personal/SlimShady/src/Frameworks/ErrorHandler.cpp" "/home/neresis/Projects/Personal/SlimShady/cmake-build-debug/CMakeFiles/SlimShady.dir/src/Frameworks/ErrorHandler.cpp.o"
+  "/home/neresis/Projects/Personal/SlimShady/src/Frameworks/FileManager.cpp" "/home/neresis/Projects/Personal/SlimShady/cmake-build-debug/CMakeFiles/SlimShady.dir/src/Frameworks/FileManager.cpp.o"
+  "/home/neresis/Projects/Personal/SlimShady/src/Frameworks/ImageComparator.cpp" "/home/neresis/Projects/Personal/SlimShady/cmake-build-debug/CMakeFiles/SlimShady.dir/src/Frameworks/ImageComparator.cpp.o"
+  "/home/neresis/Projects/Personal/SlimShady/src/Frameworks/InputHandler.cpp" "/home/neresis/Projects/Personal/SlimShady/cmake-build-debug/CMakeFiles/SlimShady.dir/src/Frameworks/InputHandler.cpp.o"
+  "/home/neresis/Projects/Personal/SlimShady/src/IndexBuffer.cpp" "/home/neresis/Projects/Personal/SlimShady/cmake-build-debug/CMakeFiles/SlimShady.dir/src/IndexBuffer.cpp.o"
+  "/home/neresis/Projects/Personal/SlimShady/src/Renderer.cpp" "/home/neresis/Projects/Personal/SlimShady/cmake-build-debug/CMakeFiles/SlimShady.dir/src/Renderer.cpp.o"
+  "/home/neresis/Projects/Personal/SlimShady/src/SceneElement.cpp" "/home/neresis/Projects/Personal/SlimShady/cmake-build-debug/CMakeFiles/SlimShady.dir/src/SceneElement.cpp.o"
+  "/home/neresis/Projects/Personal/SlimShady/src/SceneLoader.cpp" "/home/neresis/Projects/Personal/SlimShady/cmake-build-debug/CMakeFiles/SlimShady.dir/src/SceneLoader.cpp.o"
+  "/home/neresis/Projects/Personal/SlimShady/src/Shader.cpp" "/home/neresis/Projects/Personal/SlimShady/cmake-build-debug/CMakeFiles/SlimShady.dir/src/Shader.cpp.o"
+  "/home/neresis/Projects/Personal/SlimShady/src/Texture.cpp" "/home/neresis/Projects/Personal/SlimShady/cmake-build-debug/CMakeFiles/SlimShady.dir/src/Texture.cpp.o"
+  "/home/neresis/Projects/Personal/SlimShady/src/UniformManager.cpp" "/home/neresis/Projects/Personal/SlimShady/cmake-build-debug/CMakeFiles/SlimShady.dir/src/UniformManager.cpp.o"
+  "/home/neresis/Projects/Personal/SlimShady/src/VertexArray.cpp" "/home/neresis/Projects/Personal/SlimShady/cmake-build-debug/CMakeFiles/SlimShady.dir/src/VertexArray.cpp.o"
+  "/home/neresis/Projects/Personal/SlimShady/src/VertexBuffer.cpp" "/home/neresis/Projects/Personal/SlimShady/cmake-build-debug/CMakeFiles/SlimShady.dir/src/VertexBuffer.cpp.o"
+  "/home/neresis/Projects/Personal/SlimShady/src/VertexBufferLayout.cpp" "/home/neresis/Projects/Personal/SlimShady/cmake-build-debug/CMakeFiles/SlimShady.dir/src/VertexBufferLayout.cpp.o"
+  "/home/neresis/Projects/Personal/SlimShady/src/Widgets/ConsoleWidget.cpp" "/home/neresis/Projects/Personal/SlimShady/cmake-build-debug/CMakeFiles/SlimShady.dir/src/Widgets/ConsoleWidget.cpp.o"
+  "/home/neresis/Projects/Personal/SlimShady/src/Widgets/FileBrowserWidget.cpp" "/home/neresis/Projects/Personal/SlimShady/cmake-build-debug/CMakeFiles/SlimShady.dir/src/Widgets/FileBrowserWidget.cpp.o"
+  "/home/neresis/Projects/Personal/SlimShady/src/Widgets/SceneEditorWidget.cpp" "/home/neresis/Projects/Personal/SlimShady/cmake-build-debug/CMakeFiles/SlimShady.dir/src/Widgets/SceneEditorWidget.cpp.o"
+  "/home/neresis/Projects/Personal/SlimShady/src/Widgets/TaskWidget.cpp" "/home/neresis/Projects/Personal/SlimShady/cmake-build-debug/CMakeFiles/SlimShady.dir/src/Widgets/TaskWidget.cpp.o"
+  "/home/neresis/Projects/Personal/SlimShady/src/Widgets/TextEditorWidget.cpp" "/home/neresis/Projects/Personal/SlimShady/cmake-build-debug/CMakeFiles/SlimShady.dir/src/Widgets/TextEditorWidget.cpp.o"
+  "/home/neresis/Projects/Personal/SlimShady/src/Widgets/Widget.cpp" "/home/neresis/Projects/Personal/SlimShady/cmake-build-debug/CMakeFiles/SlimShady.dir/src/Widgets/Widget.cpp.o"
   )
-set(CMAKE_CXX_COMPILER_ID "MSVC")
-
-# Preprocessor definitions for this target.
-set(CMAKE_TARGET_DEFINITIONS_CXX
-  "GLEW_STATIC"
-  "IMGUI_IMPL_OPENGL_LOADER_GLEW"
-  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
 
 # The include file search paths:
 set(CMAKE_CXX_TARGET_INCLUDE_PATH
-  "../tests"
   "../Vendor"
-  "../Dependencies/GLFW/include"
-  "../Dependencies/GLEW/include"
   )
 
 # Targets to which this target links.
