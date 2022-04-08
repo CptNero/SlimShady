782bcf58e6d14f3b3db918d8a3931f2f7ca52a51
Adrian Ravai <ravaiadrian@gmail.com>
2022-04-08 13:06:52 +0200

    Added WidgetType.h

---
+++ src/WidgetType.h
@@ -0,0 +1,12 @@
+#ifndef SLIMSHADY_WIDGETTYPE_H
+#define SLIMSHADY_WIDGETTYPE_H
+
+enum class WidgetType : int {
+	TEXT_EDITOR = 0,
+	SCENE_EDITOR = 1,
+	CONSOLE = 2,
+	FILE_BROWSER = 3,
+	TASK = 4,
+};
+
+#endif //SLIMSHADY_WIDGETTYPE_H
